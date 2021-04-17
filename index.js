6c14f67e9736247ff87c0cccfac2adf9880d5547
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-17 21:27:25 +0300

    hw-05

--- index.js
+++ index.js
@@ -1,59 +1,32 @@
-// function processCall(recipient, onAvailable, onNotAvailable) {
-//   // Имитируем доступеность абонента случайным числом
-//   const isRecipientAvailable = Math.random() > 0.5;
+// const bookShelf = {
+//   authors: ["Бернард Корнуэлл", "Роберт Шекли"],
+//   getAuthors() {
+//     return this.authors;
+//   },
+//   addAuthor(authorName) {
+//     this.authors.push(authorName);
+//   },
+// };
 
-//   if (!isRecipientAvailable) {
-//     onNotAvailable(recipient);
-//     return;
-//   }
-
-//   onAvailable(recipient);
-// }
-
-// function takeCall(name) {
-//   console.log(`Соединяем с ${name}, ожидайте...`);
-//   // Логика принятия звонка
-// }
-
-// function activateAnsweringMachine(name) {
-//   console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
-//   // Логика активации автоответчика
-// }
-
-// function leaveHoloMessage(name) {
-//   console.log(`Абонент ${name} недоступен, записываем голограмму.`);
-//   // Логика записи голограммы
-// }
-
-// processCall("Манго", takeCall, activateAnsweringMachine);
-// processCall("Поли", takeCall, leaveHoloMessage);
+// console.log(bookShelf.getAuthors()); // ['Бернард Корнуэлл', 'Роберт Шекли']
+// bookShelf.addAuthor("Ли Танит");
+// console.log(bookShelf.getAuthors()); // ['Бернард Корнуэлл', 'Роберт Шекли', 'Ли Танит']
 
 const pizzaPalace = {
   pizzas: ["Ультрасыр", "Аль Копчино", "Четыре нарезона"],
-  order(pizzaName, newPizza, errorPizza) {
-    if (pizzaPalace.pizzas.includes(pizzaName)) {
-      newPizza(pizzaName);
-    } else {
-      errorPizza(`В ассортименте нет пиццы с названием ${pizzaName}.`);
-    }
+  // Пиши код ниже этой строки
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
   },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+    }
+
+    return `Заказ принят, готовим пиццу «${pizzaName}».`;
+  },
+  // Пиши код выше этой строки
 };
-// Пиши код выше этой строки
 
-  // Колбэк для onSuccess
-  function makePizza(pizzaName) {
-    console.log(`Ваш заказ принят. Готовим пиццу ${pizzaName}.`)
-    return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
-  }
-
-// Колбэк для onError
-function onOrderError(error) {
-  console.log(`Ошибка! ${error}`);
-  return `Ошибка! ${error}`;
-}
-
-// Вызовы метода с колбэками
-pizzaPalace.order("Аль Копчино", makePizza, onOrderError);
-pizzaPalace.order("Четыре нарезона", makePizza, onOrderError);
-pizzaPalace.order("Биг майк", makePizza, onOrderError);
-pizzaPalace.order("Венская", makePizza, onOrderError);
