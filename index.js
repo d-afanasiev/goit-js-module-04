bea81b7457966ec596d3c60e9ff1dd38b8dc1d86
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-17 21:22:11 +0300

    hw-04

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-// function registerGuest(name, callback) {
-//   console.log(`Регистрируем гостя ${name}.`);
-//   callback(name);
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Имитируем доступеность абонента случайным числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
 // }
 
-// // Передаём инлайн функцию greet как колбэк
-// registerGuest("Манго", function greet(name) {
-//   consle.log(`Добро пожаловать ${name}.`);
-// });
+// function takeCall(name) {
+//   console.log(`Соединяем с ${name}, ожидайте...`);
+//   // Логика принятия звонка
+// }
 
-// // Передаём инлайн функцию notify как колбэк
-// registerGuest("Поли", function notify(name) {
-//   consle.log(`Уважаемый(ая) ${name}, ваш номер будет готов через 30 минут.`);
-// });
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+//   // Логика активации автоответчика
+// }
 
-function makePizza(pizzaName, callback) {
-  console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
-  callback(pizzaName);
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+//   // Логика записи голограммы
+// }
+
+// processCall("Манго", takeCall, activateAnsweringMachine);
+// processCall("Поли", takeCall, leaveHoloMessage);
+
+const pizzaPalace = {
+  pizzas: ["Ультрасыр", "Аль Копчино", "Четыре нарезона"],
+  order(pizzaName, newPizza, errorPizza) {
+    if (pizzaPalace.pizzas.includes(pizzaName)) {
+      newPizza(pizzaName);
+    } else {
+      errorPizza(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+    }
+  },
+};
+// Пиши код выше этой строки
+
+  // Колбэк для onSuccess
+  function makePizza(pizzaName) {
+    console.log(`Ваш заказ принят. Готовим пиццу ${pizzaName}.`)
+    return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+  }
+
+// Колбэк для onError
+function onOrderError(error) {
+  console.log(`Ошибка! ${error}`);
+  return `Ошибка! ${error}`;
 }
 
-makePizza("Роял гранд", function deliverPizza(pizzaName) {
-  console.log(`Доставляем пиццу ${pizzaName}.`);
-});
-// Пиши код ниже этой строки
-
-makePizza("Ультрасыр", function eatPizza(pizzaName) {
-  console.log(`Едим пиццу ${pizzaName}`);
-});
+// Вызовы метода с колбэками
+pizzaPalace.order("Аль Копчино", makePizza, onOrderError);
+pizzaPalace.order("Четыре нарезона", makePizza, onOrderError);
+pizzaPalace.order("Биг майк", makePizza, onOrderError);
+pizzaPalace.order("Венская", makePizza, onOrderError);
