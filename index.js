50e712e219083c81814fac959d3df043182c2081
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-18 01:01:13 +0300

    hw-07

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Пиши код ниже этой строки
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * discount;
-    this.orders.push(order);
-  },
-  // Пиши код выше этой строки
-};
+// function greetGuest(greeting) {
+//   console.log(`${greeting}, ${this.username}.`);
+// }
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
+// const mango = { username: "Манго" };
+// const poly = { username: "Поли" };
+
+// greetGuest.call(mango, "Добро пожаловать");
+// // Добро пожаловать, Манго.
+
+// greetGuest.call(poly, "С приездом");
+// // С приездом, Поли.
+
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
+
+// let indexNew = 0;
+// let newArr;
+// function composeMessage(position) {
+//   console.log(
+//     `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`
+//   );
+//   // this.message(
+//   //   `Готовим ${this.dish} для ${this.email}. Ваш заказ ${index}-й в очереди.`
+//   // );
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
+
+// for (key of orders) {
+//   orders[indexNew].message = composeMessage.call(key, indexNew);
+//   indexNew += 1;
+// }
+
+//  const messages = orders.map(newArr => newArr.message);
+
+// console.log(messages);
+
+
+
+const orders = [
+  { email: "solomon@topmail.ua", dish: "Burger" },
+  { email: "artemis@coldmail.net", dish: "Pizza" },
+  { email: "jacob@mail.com", dish: "Taco" },
+];
+
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = orders.map((value, index) => composeMessage.call(value, index + 1));
+  
+
+console.log(messages);
