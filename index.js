3e262e4a0ea80bef806ac10b434b90c38ec435e5
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-17 20:25:11 +0300

    main

---
+++ index.js
@@ -0,0 +1,26 @@
+// function greet(name) {
+//   return `Добро пожаловать ${name}.`;
+// }
+
+// /* Вызываем функцию greet 
+//    и выводим результат в консоль */
+// console.log(greet("Манго"));
+// // Добро пожаловать Манго.
+
+// /* Выводим функцию greet 
+//    в консоль не вызывая её */
+// console.log(greet);
+// /* 
+// ƒ greet() { 
+//      return `Добро пожаловать ${name}.`; 
+// } 
+// */
+
+
+function makePizza() {
+  return "Ваша пицца готовится, ожидайте.";
+}
+// Пиши код ниже этой строки
+
+const result = makePizza();
+const pointer = makePizza;
